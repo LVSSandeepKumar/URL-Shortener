192904debb0f7358be49eda8913dafb1679980f7
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-29 10:22:21 +0530

    Defining the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import AppLayout from './layouts/app-layout'
+import LandingPage from './pages/landing'
+import Dashboard from './pages/dashboard'
+import Auth from './pages/auth'
+import Link from './pages/link'
+import RedirectLink from './pages/redirect-link'
+
+const router = createBrowserRouter([
+  {
+    element : <AppLayout />,
+    children : [
+      {
+        path: '/',
+        element : <LandingPage />
+      },
+      {
+        path:'/dashboard',
+        element : <Dashboard />
+      },
+      {
+        path : '/auth',
+        element : <Auth />
+      },
+      {
+        path : '/link/:id',
+        element : <Link />
+      },
+      {
+        path : '/:id',
+        element : <RedirectLink />
+      }
+    ]
+  }
+])
 
 function App() {
   
-
   return (
-    <div>
-      App
-    </div>  
+    <RouterProvider router={router} />  
   )
 }
 
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Outlet />
+      </main>
+
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made by <Link to='https://threed-web-dev-portfolio.onrender.com'>LVS Sandeep Kumar</Link>
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
