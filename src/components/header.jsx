2298b2b0fff80c3a8fabb18dcd216400b7c387f6
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-06-29 11:11:47 +0530

    Built the header

---
+++ src/components/header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import { Button } from "./ui/button";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = true;
+
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="/logo.png" className="h-20" alt="logo" />
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 overflow-hidden rounded-full">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                <span>My Links</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span className="text-red-500">LogOut</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
@@ -5,6 +6,7 @@ const AppLayout = () => {
   return (
     <div>
       <main className="min-h-screen container">
+        <Header />
         <Outlet />
       </main>
 
