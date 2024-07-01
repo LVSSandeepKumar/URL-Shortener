0403daf3db5773b70dd853776bcec5fc7b53f6cd
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-01 12:36:36 +0530

    Signup UI

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
+import Error from "./error";
+import { useEffect, useState } from "react";
+import * as Yup from "yup";
+import useFetch from "@/hooks/use-fetch";
+import { signup } from "@/db/apiAuth";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { UrlState } from "@/context/context";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [errors, setErrors] = useState([]);
+  const [formData, setFormData] = useState({
+    name:"",
+    email: "",
+    password: "",
+    profile_pic: null
+  });
 
-export default Signup
+  const navigate = useNavigate();
+  let [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+
+  const handleInputChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: files ? files[0] : value,
+    }));
+  };
+
+  const { data, error, loading, fn: fnSignup } = useFetch(signup, formData);
+  const { fetchUser } = UrlState();
+
+  useEffect(() => {
+    if (error === null && data) {
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+      fetchUser();
+    }
+  }, [error, loading]);
+
+  const handleSignup = async () => {
+    setErrors([]);
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        email: Yup.string()
+          .email("Invalid Email")
+          .required("Email is required"),
+        password: Yup.string()
+          .min(6, "Password should be minimum 6 characters long.")
+          .required("Password is required"),
+        profile_pic: Yup.mixed().required("Profile Pic is required")
+      });
+      await schema.validate(formData, { abortEarly: false });
+      await fnSignup();
+    } catch (error) {
+      const newErrors = {};
+      error?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+      setErrors(newErrors);
+    }
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Signup</CardTitle>
+        <CardDescription>Create an account if you haven't yet.</CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            onChange={handleInputChange}
+          />
+          {errors.name && <Error message={errors.name} />}
+        </div>
+        <div className="space-y-1">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+          {errors.email && <Error message={errors.email} />}
+        </div>
+        <div className="space-y-1">
+          <Input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+          {errors.password && <Error message={errors.password} />}
+        </div>
+        <div className="space-y-1">
+          <Input
+            type="file"
+            name="profile_pic"
+            accept="image/*"
+            onChange={handleInputChange}
+          />
+          {errors.profile_pic && <Error message={errors.profile_pic} />}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleSignup}>
+          {loading ? <BeatLoader size={10} /> : "Create Account"}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default Signup;
