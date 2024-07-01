d6d720523576a83a58ae2287d04ca519fe4860ba
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-01 12:46:30 +0530

    Routes protected with Authentication

---
+++ src/components/require-auth.jsx
@@ -0,0 +1,20 @@
+import { UrlState } from "@/context/context";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
+function RequireAuth({children}) {
+    const navigate = useNavigate();
+
+    const {isAuthenticated, loading} = UrlState();
+
+    useEffect(() => {
+        if(!isAuthenticated && loading == false) navigate("/auth")
+    }, [isAuthenticated, loading])
+
+    if(loading) return <BarLoader width={"100%"} />
+
+    if(isAuthenticated) return children;
+}
+
+export default RequireAuth;
