feffcd91ec2b8353fa8e1143603a30a04ce282e9
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-03 15:24:07 +0530

    Device Pie Chart for Link

--- src/components/device-info.jsx
+++ src/components/device-info.jsx
@@ -1,7 +1,46 @@
-const DeviceInfo = ({stats}) => {
-  return (
-    <div>DeviceInfo</div>
-  )
-}
+import { PieChart, Pie, ResponsiveContainer, Cell } from "recharts";
 
-export default DeviceInfo;
+const data01 = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+
+export default function DeviceInfo({ stats }) {
+
+    const deviceCount = stats.reduce((acc, item) => {
+        if(!acc[item.device]) {
+            acc[item.device] = 0;
+        }
+        acc[item.device]++;
+        return acc;
+    }, {})
+
+    const result = Object.keys(deviceCount).map((device) => ({
+        device,
+        count : deviceCount[device]
+    }));
+
+    const COLORS = ["#FFFFFF", "#E0FFFF", "#DEB887", "#DCDCDC"]
+
+  return (
+    <div style={{ width: "100%", height: 300 }}>
+      <ResponsiveContainer>
+        <PieChart width={700} height={400}>
+          <Pie
+            data={result}
+            dataKey="count"
+            labelLine={false}
+            label={({device, percent}) => 
+            `${device} : ${(percent * 100).toFixed(0)}%`}
+          >
+            {result.map((_, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
