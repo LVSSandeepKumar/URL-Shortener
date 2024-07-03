055da8959378f47b685c116d3703cd641dcc7384
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-03 13:14:03 +0530

    Link Page UI

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -1,9 +1,155 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { UrlState } from "@/context/context";
+import { getClicksForUrl } from "@/db/apiClicks";
+import { deleteUrl, getUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
+import { Copy, Download, LinkIcon, Trash } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader, BeatLoader } from "react-spinners";
 
 const Link = () => {
-  return (
-    <div>link</div>
-  )
-}
+  const downloadImage = () => {
+    const imageUrl = url?.qr;
+    const fileName = url?.title;
 
-export default Link
+    const anchor = document.createElement("a");
+    anchor.href = imageUrl;
+    anchor.download = fileName;
+
+    document.body.appendChild(anchor);
+
+    anchor.click();
+
+    document.body.removeChild(anchor);
+  };
+
+  const { id } = useParams();
+  const { user } = UrlState();
+  const navigate = useNavigate();
+
+  const {
+    loading,
+    data: url,
+    fn,
+    error,
+  } = useFetch(getUrl, { id, user_id: user?.id });
+
+  const {
+    loading: loadingStats,
+    data: stats,
+    fn: fnStats,
+  } = useFetch(getClicksForUrl, id);
+
+  const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
+
+  useEffect(() => {
+    fn();
+    fnStats();
+  }, []);
+
+  if (error) {
+    navigate("/dashboard");
+  }
+
+  let link = "";
+  if (url) {
+    link = url?.custom_url ? url?.custom_url : url.short_url;
+  }
+
+  return (
+    <>
+      {(loading || loadingStats) && (
+        <BarLoader className="mb-4" width={"100%"} color="white" />
+      )}
+      <div className="flex flex-col gap-8 sm:flex-row justify-between">
+        <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            href={`https://us.in/${link}`}
+            target="_blank"
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+          >
+            https://us.in/{link}
+          </a>
+          <a
+            href={url?.original_url}
+            target="_blank"
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+          >
+            <LinkIcon className="p-1" />
+            {url?.original_url}
+          </a>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
+          <div className="flex gap-2">
+            <Button
+              variant="ghost"
+              onClick={() =>
+                navigator.clipboard.writeText(`https://us.in/${url?.short_url}`)
+              }
+            >
+              <Copy />
+            </Button>
+            <Button variant="ghost" onClick={downloadImage}>
+              <Download />
+            </Button>
+            <Button variant="ghost" onClick={() => fnDelete()}>
+              {loadingDelete ? (
+                <BeatLoader size={5} color="white" />
+              ) : (
+                <Trash />
+              )}
+            </Button>
+          </div>
+          <img
+            src={url?.qr}
+            alt="qr code"
+            className="w-full object-contain ring ring-blue-500 self-start"
+          />
+        </div>
+
+        <Card className="sm:w-3/5">
+          <CardHeader>
+            <CardTitle className="text-4xl font-extrabold">
+              Card Title
+            </CardTitle>
+          </CardHeader>
+          {stats && stats.length ? (
+            <CardContent className="flex flex-col gap-6">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Total Clicks</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{stats?.length}</p>
+                </CardContent>
+              </Card>
+
+              <CardTitle>Location Data</CardTitle>
+              <CardTitle>DeviceInfo</CardTitle>
+            </CardContent>
+          ) : (
+            <CardContent>
+              {loadingStats === false
+                ? "No Statistics Yet"
+                : "Loading Statistics ..."}
+            </CardContent>
+          )}
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Link;
